227cff2e0a07a0dc03afacc9dc6963992d531b0a
Mustafa Prodhan <mufassagit@gmail.com>
2026-05-04 02:04:03 -0400

    more modifications

--- ecom_store/settings.py
+++ ecom_store/settings.py
@@ -12,6 +12,7 @@ DEBUG = config('DEBUG', cast=bool, default=False)
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    'dj-ecom-model-combo.onrender.com/'
 ]
 
 RENDER_HOSTNAME = config('RENDER_HOSTNAME', default='')
